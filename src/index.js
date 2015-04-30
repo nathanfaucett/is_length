ea81a189dfe304b706cc03e837397a2f36114bee
Nathan Faucett <nathanfaucett@gmail.com>
2015-04-30 12:10:24 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 var MAX_SAFE_INTEGER = Math.pow(2, 53) - 1;
 
 
-module.exports = function isLength(obj) {
+module.exports = isLength;
+
+
+function isLength(obj) {
     return typeof(obj) === "number" && obj > -1 && obj % 1 === 0 && obj <= MAX_SAFE_INTEGER;
-};
+}
